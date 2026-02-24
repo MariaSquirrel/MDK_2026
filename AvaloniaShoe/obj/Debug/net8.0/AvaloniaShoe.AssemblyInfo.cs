96ea05764f79661204ebd039d23ea80fc0cee966
Maria <maria@example.com>
2026-02-24 13:03:24 +0300

    commit

--- AvaloniaShoe/obj/Debug/net8.0/AvaloniaShoe.AssemblyInfo.cs
+++ AvaloniaShoe/obj/Debug/net8.0/AvaloniaShoe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaShoe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbef181c4192d9d0bbdcfa6be352ce4d9c76232e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaShoe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaShoe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
